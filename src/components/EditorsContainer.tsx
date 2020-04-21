f2d6312bbb85d72dd2a4c6f2f7e8e64168db79ed
RiinaVi <riinavi86@gmail.com>
2020-04-21 16:45:24 +0300

    fix async code

--- src/components/EditorsContainer.tsx
+++ src/components/EditorsContainer.tsx
@@ -8,30 +8,45 @@ import Editor from "./Editor";
 const EditorsContainer = () => {
 
     const defaultJson = '{"a":1}';
-    const defaultJS = 'console.log(SESSION.a)';
+    const defaultJS = 'fetch("http://64.227.78.13:1001")\n' +
+        '.then(function(response) {\n' +
+        '    // response.text() возвращает новый промис,\n' +
+        '    // который выполняется и возвращает полный ответ сервера,\n' +
+        '    // когда он загрузится\n' +
+        '\n' +
+        '    return response.text();\n' +
+        '  })\n' +
+        '  .then(function(text) {\n' +
+        '    // ...и здесь содержимое полученного файла\n' +
+        '    console.log(text);\n' +
+        '    //alert(text); // {"name": "iliakan", isAdmin: true}\n' +
+        '  });\n' +
+        '// console.log(SESSION.a + Math.max(10, 20))';
 
     const [jsonCode, setJsonCode] = useState(defaultJson);
     const [jsCode, setJsCode] = useState(defaultJS);
     const [res, setRes] = useState('');
     const [hasError, setError] = useState(false);
 
-    let logs: any = [];
+    let logs: Array<string> = [];
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    function customLog(code:string) {
-        console.log(code);
+    function customLog(code: string) {
+        // console.log(code);
         logs.push(code);
     }
 
-    function getResult() {
+    async function getResult() {
         setError(false);
         try {
 
             // eslint-disable-next-line no-eval,@typescript-eslint/no-unused-vars
             let SESSION = JSON.parse(jsonCode);
             // eslint-disable-next-line no-eval
-            eval(`${duplicateConsoleLog(jsCode)}`);
+            await eval(`${duplicateConsoleLog(jsCode)}`);
             setRes(logs.join('\n'));
+            console.log(logs);
+            logs = [];
 
         } catch (e) {
             setError(true);

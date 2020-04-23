703ce1497c015d443e432c82ac628ee09f4b5c90
RiinaVi <riinavi86@gmail.com>
2020-04-23 13:56:13 +0300

    testing prototype

---
+++ test/editor-test.js
@@ -0,0 +1,15 @@
+// We use the assert standard library to make assertions
+const assert = require('assert')
+// const { add, subtract } = require('./calculator')
+// const EditorsContainer = require('../src/components/EditorsContainer')
+const {evaluateResult} = require('../src/utils/getResult');
+
+
+// We do not need to import the test functions since
+// they are made global variables by test.js
+test('should return', async () => {
+    const jsonCode = '{"a":1}';
+    const jsCode = 'console.log(SESSION.a);';
+    assert.equal(await evaluateResult(jsonCode, jsCode), '1');
+})
+
